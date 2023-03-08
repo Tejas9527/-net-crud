1cffc876ccc22a1da18890cb1dad68b354f0a8cb
Tejas9527 <121743115+Tejas9527@users.noreply.github.com>
2023-03-08 16:18:53 +0530

    crud

---
+++ Application_crud/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Application_crud.Models
+{
+    public class Employee
+    {
+        [Key]
+        [Required]
+        [DisplayName("EMP ID")]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("ProductName")]
+        public string ProductName { get; set; }
+        [Required]
+        [DisplayName("ProductId")]
+        public string ProductId { get; set; }
+        [Required]
+        [DisplayName("categryId")]
+        public int categryId { get; set; }
+        [Required]
+        [DisplayName("categryName")]
+        public string categryName { get; set; }
+
+    }
+}
